474713bf952b4a9f7336aa50694a33f93b4244f2
Alex Meijer <asure007@gmail.com>
2011-05-08 03:45:29 +0200

    Revert 067adbcb8596763a953b and re-do it properly.

--- drivers/video/samsung/s3cfb_at070tn90.c
+++ drivers/video/samsung/s3cfb_at070tn90.c
@@ -1,9 +1,6 @@
-/* linux/drivers/video/samsung/s3cfb_lte480wv.c
+/* linux/drivers/video/samsung/s3cfb_at070tn90.c
  *
- * Copyright (c) 2010 Samsung Electronics Co., Ltd.
- *		http://www.samsung.com/
- *
- * LTE480 4.8" WVGA Landscape LCD module driver for the SMDK
+ * Innolux at070tn90 7" LCD module driver for the SMDK
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -12,21 +9,21 @@
 
 #include "s3cfb.h"
 
-static struct s3cfb_lcd lb070wv6 = {
+static struct s3cfb_lcd at070tn90 = {
 	.width	= 800,
 	.height	= 480,
 	.bpp	= 32,
-	.freq	= 65,
+	.freq	= 60,
 
 	.timing = {
-		.h_fp	= 64,
-		.h_bp	= 64,
-		.h_sw	= 128,
-		.v_fp	= 16,
+		.h_fp	= 10,
+		.h_bp	= 78,
+		.h_sw	= 10,
+		.v_fp	= 30,
 		.v_fpe	= 1,
-		.v_bp	= 14,
+		.v_bp	= 30,
 		.v_bpe	= 1,
-		.v_sw	= 1,
+		.v_sw	= 2,
 	},
 
 	.polarity = {
@@ -37,9 +34,16 @@ static struct s3cfb_lcd lb070wv6 = {
 	},
 };
 
+/* static void lcd_init(void)
+{
+write_power_item_value(POWER_LCD, 1);
+write_power_item_value(POWER_BACKLIGHT, 1);
+}
+*/
+
 /* name should be fixed as 's3cfb_set_lcd_info' */
 void s3cfb_set_lcd_info(struct s3cfb_global *ctrl)
 {
-	lb070wv6.init_ldi = NULL;
-	ctrl->lcd = &lb070wv6;
+	at070tn90.init_ldi = NULL;
+	ctrl->lcd = &at070tn90;
 }
